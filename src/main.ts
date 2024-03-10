f04aaceb314e3c3c1433ebd8614b3b1bb38c59fb
Zoe <git@zoe-codez.xyz>
2024-03-10 10:46:05 -0500

    code divide

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import { CreateApplication } from "@digital-alchemy/core";
+import { LIB_HASS } from "@digital-alchemy/core/hass";
+
+import { BuildTypes } from "./build.extension";
+import { TypeWriter } from "./type-writer.extension";
+
+export const TYPE_WRITER = CreateApplication({
+  configuration: {
+    TARGET_FILE: {
+      description:
+        "Define a file to write types to. Autodetect = default behavior",
+      type: "string",
+    },
+  },
+  libraries: [LIB_HASS],
+  name: "type_writer",
+  services: {
+    build: BuildTypes,
+    type_writer: TypeWriter,
+  },
+});
+setImmediate(async () => {
+  await TYPE_WRITER.bootstrap({
+    configuration: {
+      boilerplate: {
+        LOG_LEVEL: "warn",
+      },
+      hass: {
+        AUTO_CONNECT_SOCKET: false,
+        AUTO_SCAN_CALL_PROXY: false,
+      },
+    },
+  });
+});
+
+declare module "@digital-alchemy/core" {
+  export interface LoadedModules {
+    type_writer: typeof TYPE_WRITER;
+  }
+}
