ef937e13620b1e30b05d90d9503369da661774fa
Zoe Codez <github@zoe-codez.tools>
2024-04-15 21:09:47 -0500

    default to string

--- src/identifiers.extension.ts
+++ src/identifiers.extension.ts
@@ -1,62 +1,76 @@
-import { TServiceParams } from "@digital-alchemy/core";
-import { factory } from "typescript";
+import { is, TServiceParams } from "@digital-alchemy/core";
+import { factory, SyntaxKind } from "typescript";
 
 export function Identifiers({ hass, type_writer }: TServiceParams) {
   return {
     area() {
       return type_writer.printer(
         "TAreaId",
-        factory.createUnionTypeNode(
-          hass.area.current.map(i =>
-            factory.createLiteralTypeNode(
-              factory.createStringLiteral(i.area_id),
+        is.empty(hass.area.current)
+          ? factory.createKeywordTypeNode(SyntaxKind.StringKeyword)
+          : factory.createUnionTypeNode(
+              hass.area.current.map(i =>
+                factory.createLiteralTypeNode(
+                  factory.createStringLiteral(i.area_id),
+                ),
+              ),
             ),
-          ),
-        ),
       );
     },
     device() {
       return type_writer.printer(
         "TDeviceId",
-        factory.createUnionTypeNode(
-          hass.device.current.map(i =>
-            factory.createLiteralTypeNode(factory.createStringLiteral(i.id)),
-          ),
-        ),
+        is.empty(hass.device.current)
+          ? factory.createKeywordTypeNode(SyntaxKind.StringKeyword)
+          : factory.createUnionTypeNode(
+              hass.device.current.map(i =>
+                factory.createLiteralTypeNode(
+                  factory.createStringLiteral(i.id),
+                ),
+              ),
+            ),
       );
     },
     floor() {
       return type_writer.printer(
         "TFloorId",
-        factory.createUnionTypeNode(
-          hass.floor.current.map(i =>
-            factory.createLiteralTypeNode(
-              factory.createStringLiteral(i.floor_id),
+        is.empty(hass.floor.current)
+          ? factory.createKeywordTypeNode(SyntaxKind.StringKeyword)
+          : factory.createUnionTypeNode(
+              hass.floor.current.map(i =>
+                factory.createLiteralTypeNode(
+                  factory.createStringLiteral(i.floor_id),
+                ),
+              ),
             ),
-          ),
-        ),
       );
     },
     label() {
       return type_writer.printer(
         "TLabelId",
-        factory.createUnionTypeNode(
-          hass.label.current.map(i =>
-            factory.createLiteralTypeNode(
-              factory.createStringLiteral(i.label_id),
+        is.empty(hass.label.current)
+          ? factory.createKeywordTypeNode(SyntaxKind.StringKeyword)
+          : factory.createUnionTypeNode(
+              hass.label.current.map(i =>
+                factory.createLiteralTypeNode(
+                  factory.createStringLiteral(i.label_id),
+                ),
+              ),
             ),
-          ),
-        ),
       );
     },
     zone() {
       return type_writer.printer(
         "TZoneId",
-        factory.createUnionTypeNode(
-          hass.zone.current.map(i =>
-            factory.createLiteralTypeNode(factory.createStringLiteral(i.id)),
-          ),
-        ),
+        is.empty(hass.zone.current)
+          ? factory.createKeywordTypeNode(SyntaxKind.StringKeyword)
+          : factory.createUnionTypeNode(
+              hass.zone.current.map(i =>
+                factory.createLiteralTypeNode(
+                  factory.createStringLiteral(i.id),
+                ),
+              ),
+            ),
       );
     },
   };
